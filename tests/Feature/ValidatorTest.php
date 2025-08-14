0ec703138f77276fea609a938a9e7761a58864ee
Haiiyusss <128706837+dhayussyahri@users.noreply.github.com>
2025-08-14 15:10:29 +0700

    Belajar menambahkan Error Message dengan unit test

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
@@ -44,5 +45,9 @@ class ValidatorTest extends TestCase
 
         self::assertFalse($validator->passes());
         self::assertTrue($validator->fails());
+
+        $message = $validator->getMessageBag();
+
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
     }
 }
