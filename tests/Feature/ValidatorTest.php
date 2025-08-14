3d984475ced409daba958d5cc54c37ea6868a89c
Haiiyusss <128706837+dhayussyahri@users.noreply.github.com>
2025-08-14 16:01:50 +0700

    Belajar menambahkan Validation Message

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -2,12 +2,13 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Validation\ValidationException;
-use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ValidatorTest extends TestCase
 {
@@ -79,6 +80,8 @@ class ValidatorTest extends TestCase
 
     public function testValidatorMultipleRules()
     {
+        App::setLocale("id");
+
         $data = [
             "username" => "edi",
             "password" => "edi"
@@ -125,5 +128,35 @@ class ValidatorTest extends TestCase
             $message = $exception->validator->errors();
             Log::error($message->toJson(JSON_PRETTY_PRINT));
         }
+    }
+
+    public function testValidatorInlinesMessage()
+    {
+        $data = [
+            "username" => "edi",
+            "password" => "edi"
+        ];
+
+        $rules = [
+            "username" => "required|email|max:100",
+            "password" => ["required", "min:6", "max:20"]
+        ];
+
+        $message = [
+            "required" => ":attribute harus diisi",
+            "email" => ":attribute harus berupa email",
+            "min" => ":attribute minimal :min karakter",
+            "max" => ":attribute maximal :max karakter",
+        ];
+
+        $validator = Validator::make($data, $rules, $message);
+        self::assertNotNull($validator);
+
+        self::assertFalse($validator->passes());
+        self::assertTrue($validator->fails());
+
+        $message = $validator->getMessageBag();
+
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
     }
 }
