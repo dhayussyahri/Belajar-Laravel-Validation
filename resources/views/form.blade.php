4a615c3ed68c166abd8fbcef5e9e6f385529595d
Haiiyusss <128706837+dhayussyahri@users.noreply.github.com>
2025-08-17 21:21:12 +0700

    Belajar menambahkan Repopulating Forms

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -17,8 +17,10 @@
 
    <form action="/form" method="post">
       @csrf
-      <label> Username : @error('username') {{ $message }} @enderror <input type="text" name="username"></label><br>
-      <label> Password : @error('password') {{ $message }}@enderror<input type="password" name="password"></label><br>
+      <label> Username : @error('username') {{ $message }} @enderror
+         <input type="text" name="username" value="{{ old('username') }}"></label><br>
+      <label> Password : @error('password') {{ $message }}@enderror
+         <input type="password" name="password" value="{{ old('password') }}"></label><br>
       <input type="submit" value="Login">
    </form>
 
